fda480024d77543066788dbbd2e8909a751732ba
Armankha <36308098+Armankha@users.noreply.github.com>
2019-04-21 21:05:12 +0600

    1

---
+++ app/constants.js
@@ -0,0 +1,12 @@
+define(['require'], function(require) {
+    return {
+        /**
+         * Application constants. This values generated from all service settings.
+         * Also there are Push Notification settings there if Push Notifications enabled for this project.
+         */
+        /**
+         * Settings
+         */
+        Settings: {}
+    };
+});
